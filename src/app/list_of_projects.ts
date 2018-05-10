d8d1683198dc20dc6933e73019ba68c80f46f0a3
Cesar <tratohecho3@gmail.com>
2018-05-09 20:17:44 -0400

    projects created

---
+++ src/app/list_of_projects.ts
@@ -0,0 +1,5 @@
+import { Project } from './project';
+
+export const PROJECTS: Project[] = [
+    {title:'My Portfolio', description:'I created from scratch my own Web Site', image:"./assets/my_portfolio.jpg",type_of_project:'Frontend Development',techologies:['html','css','javascript','angular','bootstrap']}
+]
---
+++ src/app/project.ts
@@ -0,0 +1,8 @@
+export class Project{
+    title:string;
+    description:string;
+    image:string;
+    type_of_project:string;
+    techologies:any[];
+
+}
