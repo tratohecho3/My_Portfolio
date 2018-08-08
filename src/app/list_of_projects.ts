79835ef592663b5d68a495a09664171104bb2800
Cesar <tratohecho3@gmail.com>
2018-08-07 23:00:38 -0400

    about fixed

--- src/app/list_of_projects.ts
+++ src/app/list_of_projects.ts
@@ -3,6 +3,7 @@ import { Project } from './project';
 export const PROJECTS: Project[] = [
     {title:'My Portfolio', description:'I created from scratch my own Web Site', image:"./assets/project1.jpg",type_of_project:'Frontend Development',techologies:['html','css','javascript','angular','bootstrap']},
     {title:'My Portfolio2', description:'I created from scratch my own Web Site2', image:"./assets/project2.jpg",type_of_project:'Frontend Development',techologies:['html','css','javascript','angular','bootstrap']},
-    {title:'My Portfolio2', description:'I created from scratch my own Web Site2', image:"./assets/project3.jpg",type_of_project:'Frontend Development',techologies:['html','css','javascript','angular','bootstrap']},
-    {title:'My Portfolio2', description:'I created from scratch my own Web Site2', image:"./assets/project4.jpg",type_of_project:'Frontend Development',techologies:['html','css','javascript','angular','bootstrap']}
+    {title:'My Portfolio3', description:'I created from scratch my own Web Site3', image:"./assets/project3.jpg",type_of_project:'Frontend Development',techologies:['html','css','javascript','angular','bootstrap']},
+    {title:'My Portfolio4', description:'I created from scratch my own Web Site4', image:"./assets/project4.jpg",type_of_project:'Frontend Development',techologies:['html','css','javascript','angular','bootstrap']},
+    
 ]
