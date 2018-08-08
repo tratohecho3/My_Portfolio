f0b43314da57bc5d653155052b003dd87d73bb3a
Cesar <tratohecho3@gmail.com>
2018-08-08 12:15:17 -0400

    about timeline added

--- src/app/list_of_projects.ts
+++ src/app/list_of_projects.ts
@@ -5,5 +5,6 @@ export const PROJECTS: Project[] = [
     {title:'My Portfolio2', description:'I created from scratch my own Web Site2', image:"./assets/project2.jpg",type_of_project:'Frontend Development',techologies:['html','css','javascript','angular','bootstrap']},
     {title:'My Portfolio3', description:'I created from scratch my own Web Site3', image:"./assets/project3.jpg",type_of_project:'Frontend Development',techologies:['html','css','javascript','angular','bootstrap']},
     {title:'My Portfolio4', description:'I created from scratch my own Web Site4', image:"./assets/project4.jpg",type_of_project:'Frontend Development',techologies:['html','css','javascript','angular','bootstrap']},
+    {title:'My Portfolio4', description:'I created from scratch my own Web Site4', image:"./assets/project4.jpg",type_of_project:'Frontend Development',techologies:['html','css','javascript','angular','bootstrap']},
     
 ]
--- src/app/list_of_works.ts
+++ src/app/list_of_works.ts
@@ -24,8 +24,65 @@ image: "./assets/usb.png",
 date: 'August 2017 – December 2017 (5 months)',
 address: 'Florida, Estados Unidos'
 
+},
+{title:'Intern Student', description: `Machine Learning. 
+Learning and helping in a group of student(PHD) researchers
+Technologies:
+• Python
+• Pytorch 
+• TensorFlow
+• Git / Github
+
+I worked in The Roitberg Research Group
+https://roitberg.chem.ufl.edu`,
+image: "./assets/usb.png", 
+date: 'August 2017 – December 2017 (5 months)',
+address: 'Florida, Estados Unidos'
+
+},
+{title:'Intern Student', description: `Machine Learning. 
+Learning and helping in a group of student(PHD) researchers
+Technologies:
+• Python
+• Pytorch 
+• TensorFlow
+• Git / Github
+
+I worked in The Roitberg Research Group
+https://roitberg.chem.ufl.edu`,
+image: "./assets/usb.png", 
+date: 'August 2017 – December 2017 (5 months)',
+address: 'Florida, Estados Unidos'
+
+},
+{title:'Intern Student', description: `Machine Learning. 
+Learning and helping in a group of student(PHD) researchers
+Technologies:
+• Python
+• Pytorch 
+• TensorFlow
+• Git / Github
+
+I worked in The Roitberg Research Group
+https://roitberg.chem.ufl.edu`,
+image: "./assets/usb.png", 
+date: 'August 2017 – December 2017 (5 months)',
+address: 'Florida, Estados Unidos'
+
+},
+{title:'Intern Student', description: `Machine Learning. 
+Learning and helping in a group of student(PHD) researchers
+Technologies:
+• Python
+• Pytorch 
+• TensorFlow
+• Git / Github
+
+I worked in The Roitberg Research Group
+https://roitberg.chem.ufl.edu`,
+image: "./assets/usb.png", 
+date: 'August 2017 – December 2017 (5 months)',
+address: 'Florida, Estados Unidos'
+
 }
-
-
-
 ]
