b3ef143b61f3624b0a4b32e1168da4f9bfcfc498
Cesar <tratohecho3@gmail.com>
2018-08-14 21:15:45 -0400

    padding implemented

--- src/app/list_volunteer_experience.ts
+++ src/app/list_volunteer_experience.ts
@@ -2,15 +2,15 @@
 
 export const volunteer= [
     {title:'Vice president of the Student Center', place: `Colégio La Salle`,
-    image: "./assets/uf.jpg", 
+    image: "./assets/salle.jpg", 
     date: 'September 2012 – July 2013',
     description: 'Main organization of the students of the Colegio La Salle Tienda Honda in Caracas, Venezuela'},
     {title:'Principal Delegate Student Center', place: `Universidad Simón Bolívar`,
-    image: "./assets/uf.jpg", 
+    image: "./assets/usb.png", 
     date: 'September 2013 – September 2014',
     description: 'Main organization of the students of cohort 13 of Simon Bolivar University in Caracas, Venezuela'},
     {title:'Founder and President Student Group', place: `Universidad Simón Bolívar`,
-    image: "./assets/uf.jpg", 
+    image: "./assets/calistenia.jpg", 
     date: 'September 2014 – September 2016',
     description: `Calistenia USB is a sports student group created in the year 2014, 
     at the service of students for the improvement of physical and mental condition. 
