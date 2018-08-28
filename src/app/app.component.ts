a74dc0a1a8fc4c33fe7791e0c86b840e741d9f0b
Cesar <tratohecho3@gmail.com>
2018-08-28 10:58:12 -0400

    navbar scroll implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,34 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   isCollapsed = true;
+  public ngOnInit() {
+    console.log('entre')
+    $(document).ready(function(){       
+      var scroll_start = 0;
+      var startchange = $('#startchange');
+      var offset = startchange.offset();
+       if (startchange.length){
+        console.log('entre2')
 
+      $(document).scroll(function() { 
+         scroll_start = $(this).scrollTop();
+         console.log('entre3')
 
-  toggle(element) {
-
+         if(scroll_start > offset.top) {
+             $(".navbar-dark").css('background-color', 'black');
+             console.log('entre4')
+          } else {
+             $('.navbar-dark').css('background-color', 'transparent');
+             console.log('entre5')
+          }
+      });
+       }
+   });
   }
 }
