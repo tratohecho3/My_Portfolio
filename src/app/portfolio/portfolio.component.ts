7336955eb23c992978724a2096af7b0cabe33620
Cesar <tratohecho3@gmail.com>
2018-08-31 15:05:39 -0400

    some animations

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Project } from '../project';
-import { PROJECTS} from '../list_of_projects';
 import { ProjectsService } from '../services/projects.service';
+import { trigger, transition, useAnimation } from '@angular/animations';
+
+ 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.css']
+  styleUrls: ['./portfolio.component.css'],
+
 })
 export class PortfolioComponent implements OnInit {
   projects: Project[];
+
   constructor(private projectService: ProjectsService) { 
     this.projects = projectService.getProjects();
   }
