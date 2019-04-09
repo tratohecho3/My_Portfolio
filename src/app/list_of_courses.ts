f34ee1239632f1c2e9b8c273e612ff0ed90d426d
Cesarc <cesar.colina@actionmediamtl.com>
2019-04-09 15:21:32 -0400

    eliminando detalles

--- src/app/list_of_courses.ts
+++ src/app/list_of_courses.ts
@@ -1,9 +1,6 @@
 import { Course } from './course';
 
 export const COURSES: Course[] = [
-    {title:'Typescript 2019 Esencial', description:'El curso mas completo antes de Angular', image:"./assets/typescript.png",teachable:"", udemy:"", youtube: ""},
-    {title:'Typescript 2019 Esencial', description:'El curso mas completo antes de Angular', image:"./assets/typescript.png",teachable:"", udemy:"", youtube: ""},
     {title:'Typescript 2019 Esencial', description:'El curso mas completo antes de Angular', image:"./assets/typescript.png",teachable:"", udemy:"", youtube: ""}
-
 
 ]
