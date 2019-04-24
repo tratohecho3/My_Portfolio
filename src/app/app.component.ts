b93a87d0732181525e5cc2e066af8020fb4a6df8
Cesarc <cesar.colina@actionmediamtl.com>
2019-04-24 11:43:01 -0400

    agregando navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,22 +12,28 @@ export class AppComponent implements OnInit {
       var scroll_start = 0;
       var startchange = $('#startchange');
       var offset = startchange.offset();
-       if (startchange.length){
-        scroll_start = $(this).scrollTop();
-       if(scroll_start > offset.top) {
-           $(".navbar-dark").css('background-color', 'black');
-        } else {
-           $('.navbar-dark').css('background-color', 'transparent');
-        }
-      $(document).scroll(function() { 
-         scroll_start = $(this).scrollTop();
+      if($(document).width() < 1200) {
+        $(".navbar-dark").css('background-color', 'black');
+      }
+      else {
+        if (startchange.length){
+          scroll_start = $(this).scrollTop();
          if(scroll_start > offset.top) {
              $(".navbar-dark").css('background-color', 'black');
           } else {
              $('.navbar-dark').css('background-color', 'transparent');
           }
-      });
-       }
+        $(document).scroll(function() { 
+           scroll_start = $(this).scrollTop();
+           if(scroll_start > offset.top) {
+               $(".navbar-dark").css('background-color', 'black');
+            } else {
+               $('.navbar-dark').css('background-color', 'transparent');
+            }
+        });
+         }
+      }
+
    });
   }
 }
