82ae55db5885496e68104254b5d8ca2158495832
Cesar <tratohecho3@gmail.com>
2019-09-24 21:53:12 -0400

    cupon

--- src/app/list_of_courses.ts
+++ src/app/list_of_courses.ts
@@ -1,7 +1,7 @@
 import { Course } from './course';
 
 export const COURSES: Course[] = [
-    {id:0, password:'clave123', title:'Typescript 2019 Esencial', description:'El curso mas completo antes de Angular', image:"./assets/curso_TS2.jpg",teachable:"https://academia-web.teachable.com/p/typescript", udemy:"https://www.udemy.com/curso-de-typescript-2019-esencial-el-curso-antes-de-angular/?couponCode=PAGINAWEB", youtube: ""},
+    {id:0, password:'clave123', title:'Typescript 2019 Esencial', description:'El curso mas completo antes de Angular', image:"./assets/curso_TS2.jpg",teachable:"https://www.academiaweb.ca/p/typescript/?coupon_code=PAGINAWEB", udemy:"https://www.udemy.com/curso-de-typescript-2019-esencial-el-curso-antes-de-angular/?couponCode=PAGINAWEB", youtube: ""},
     {id:1, password:'clave123', title:'Introduccion a la programacion: GIT y Terminal', description:'Inicia en el mundo de la programacion', image:"./assets/curso_herramientas_gratis.jpg",teachable:"https://www.academiaweb.ca/p/introduccion-a-la-programacion-git-terminal", udemy:"none", youtube: ""}
 
     
