143a6d0048717460c770a7a56fa2a673a7ab3197
Cesar <tratohecho3@gmail.com>
2020-05-31 17:37:40 -0400

    add curso css

--- src/app/list_of_courses.ts
+++ src/app/list_of_courses.ts
@@ -1,48 +1,63 @@
-import { Course } from './course';
+import { Course } from "./course";
 
 export const COURSES: Course[] = [
-    {
-        id: 0,
-        password: 'clave123',
-        title: 'Typescript 2019 Esencial',
-        description: 'El curso mas completo antes de Angular',
-        image: "./assets/curso_TS2.jpg",
-        teachable: "https://cursos.academiaweb.ca/p/typescript/?coupon_code=PAGINAWEB",
-        udemy: "https://www.udemy.com/course/curso-de-typescript-2019-esencial-el-curso-antes-de-angular/?referralCode=41B1A84B9C10D5C30568",
-        youtube: ""
-    },
+  {
+    id: 0,
+    password: "clave123",
+    title: "Typescript 2019 Esencial",
+    description: "El curso mas completo antes de Angular",
+    image: "./assets/curso_TS2.jpg",
+    teachable:
+      "https://cursos.academiaweb.ca/p/typescript/?coupon_code=PAGINAWEB",
+    udemy:
+      "https://www.udemy.com/course/curso-de-typescript-2019-esencial-el-curso-antes-de-angular/?referralCode=41B1A84B9C10D5C30568",
+    youtube: ""
+  },
 
-    {
-        id: 1,
-        password: 'clave123',
-        title: 'Introduccion a la programacion: GIT y Terminal',
-        description: 'Inicia en el mundo de la programacion',
-        image: "./assets/curso_herramientas_gratis.jpg",
-        teachable: "https://cursos.academiaweb.ca/p/introduccion-a-la-programacion-git-terminal",
-        udemy: "none",
-        youtube: ""
-    },
+  {
+    id: 1,
+    password: "clave123",
+    title: "Introduccion a la programacion: GIT y Terminal",
+    description: "Inicia en el mundo de la programacion",
+    image: "./assets/curso_herramientas_gratis.jpg",
+    teachable:
+      "https://cursos.academiaweb.ca/p/introduccion-a-la-programacion-git-terminal",
+    udemy: "none",
+    youtube: ""
+  },
 
-    {
-        id: 2,
-        password: 'clave123',
-        title: 'Git y Github Profesional',
-        description: 'Domina el Sistema de Control de Versiones',
-        image: "./assets/curso_git_github.jpg",
-        teachable: "https://cursos.academiaweb.ca/p/git-y-github-profesional/?coupon_code=PAGINAWEB",
-        udemy: "https://www.udemy.com/course/curso-de-git-y-github-git-profesional-desde-cero/?referralCode=82C485B01B3E73BFD078",
-        youtube: ""
-    },
-    {
-        id: 3,
-        password: 'clave123',
-        title: 'HTML: Introduccion al Desarrollo Web (HTML5)',
-        description: 'Aprende Desarrollo Web desde cero.',
-        image: "./assets/curso_html.jpg",
-        teachable: "https://cursos.academiaweb.ca/p/html/?coupon_code=PAGINAWEB",
-        udemy: "https://www.udemy.com/course/html-introduccion-al-desarrollo-web-html5/?referralCode=89B3689C1EA70595B6B4",
-        youtube: ""
-    }
-
-
-]
+  {
+    id: 2,
+    password: "clave123",
+    title: "Git y Github Profesional",
+    description: "Domina el Sistema de Control de Versiones",
+    image: "./assets/curso_git_github.jpg",
+    teachable:
+      "https://cursos.academiaweb.ca/p/git-y-github-profesional/?coupon_code=PAGINAWEB",
+    udemy:
+      "https://www.udemy.com/course/curso-de-git-y-github-git-profesional-desde-cero/?referralCode=82C485B01B3E73BFD078",
+    youtube: ""
+  },
+  {
+    id: 3,
+    password: "clave123",
+    title: "HTML: Introduccion al Desarrollo Web (HTML5)",
+    description: "Aprende Desarrollo Web desde cero.",
+    image: "./assets/curso_html.jpg",
+    teachable: "https://cursos.academiaweb.ca/p/html/?coupon_code=PAGINAWEB",
+    udemy:
+      "https://www.udemy.com/course/html-introduccion-al-desarrollo-web-html5/?referralCode=89B3689C1EA70595B6B4",
+    youtube: ""
+  },
+  {
+    id: 4,
+    password: "clave123",
+    title: "Curso de CSS: Crea Paginas Web con HTML y CSS",
+    description: "Crea un Clon de Netflix",
+    image: "./assets/curso_css.jpg",
+    teachable: "https://cursos.academiaweb.ca/p/css/?coupon_code=PAGINAWEB",
+    udemy:
+      "https://www.udemy.com/course/curso-de-ccs-crea-paginas-web-con-html-y-css/?referralCode=B8FF8F15F5BA7913592D",
+    youtube: ""
+  }
+];
